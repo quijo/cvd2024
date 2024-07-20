60c7129c27453142521d9956c57852404daee339
John Quiachon <rjohnquia@gmail.com>
2024-07-20 13:45:00 +0800

    setup hero

---
+++ components/hero.php
@@ -0,0 +1,24 @@
+<div>
+<div class="flex justify-center items-center h-screen herobg">
+     
+     <!-- <p class="text-red-600 text-4xl"> 
+          This site is underconstruction
+      </p>  -->
+
+      <div class="opacitybgcolor">
+        <?php include 'components/header.php';?>
+        <div class="flex justify-around">
+            <div class="text text-white ml-16 flex-1 flex-col justify-center items-center">
+                <h1 class="flex-col text-5xl font-bold flex-1 mt-20">Making Christlike Disciple</h1>
+                <p class="mt-10">The Church of the Nazarene is dedicated to making Christlike disciples by fostering a community deeply rooted in faith, love, and service. This involves guiding individuals to develop a personal relationship with Jesus Christ, nurturing spiritual growth through worship, education, and fellowship, and empowering members to live out their faith through acts of compassion, justice, and evangelism. The church emphasizes holiness of heart and life, encouraging believers to embody Christ's teachings in their daily interactions and to actively participate in God's mission to transform the world.</p>
+
+                <button class="mt-5 bg-pink-600 py-2 px-4 rounded-md">Contact Us</button>
+            </div>
+            <div class="image flex-1"></div>
+
+         
+        </div>
+        <div class="flex justify-center text-3xl text-red-600 mt-10 italic"><p>"This site is underconstruction"</p></div>
+      </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -6,20 +6,16 @@
     <title>CVD</title>
     <link rel="stylesheet" href="./src/output.css">
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link rel="stylesheet" href="./src/style.css">
 </head>
 <body>
 <!-- header with menu-->
-    <header class="bg-blue-400">
-        <?php include 'components/header.php';?>
-    </header>
+
+       
 
 <!-- hero -->
-    <div class="flex justify-center items-center h-screen ">
-     
-       <p class="text-red-600 text-4xl"> 
-            This site is underconstruction
-        </p> 
-    </div>
+     <?php include 'components/hero.php' ?>
+   
     
 </body>
 </html>
