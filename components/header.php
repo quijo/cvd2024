86dbaba6be41b6ed0266dbdc65371ccff90a1d54
John Quiachon <rjohnquia@gmail.com>
2024-07-20 14:04:43 +0800

    clean

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 
 <div class="flex justify-between items-center px-16 py-4  top-0  text-white bg-slate-500">
     <div class=" flex flex-col logo justify-center items-center ">
-        <img class="w-[240px]"src="images/logo2.png"   alt="Church of the nazaerene logo" />
+        <img class="w-[240px]" src="images/logo.png"   alt="Church of the nazaerene logo" />
         <p class="">Central Visayas District.</p>
     </div>
     <?php include 'nav.php' ?>
