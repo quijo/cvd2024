e92efcfd782d17d99d9bdd3d74fb9dc93da93467
John Quiachon <rjohnquia@gmail.com>
2024-07-20 12:43:36 +0800

    correct central spelling

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <div class="flex justify-between items-center px-16 text-white py-4">
     <div class=" flex flex-col logo justify-center items-center ">
         <img class="w-[240px]"src="images/logo2.png"   alt="Church of the nazaerene logo" />
-        <p class="">Centeral Visayas District.</p>
+        <p class="">Central Visayas District.</p>
     </div>
     <?php include 'nav.php' ?>
 </div>
