bfba2bc8b559a1d5c3c337ac84868257e7968cfc
John Quiachon <rjohnquia@gmail.com>
2024-07-20 13:58:28 +0800

    fix bug 1

--- components/hero.php
+++ components/hero.php
@@ -6,7 +6,7 @@
       </p>  -->
 
       <div class="opacitybgcolor">
-        <?php include 'components/header.php';?>
+
         <div class="flex justify-around">
             <div class="text text-white ml-16 flex-1 flex-col justify-center items-center">
                 <h1 class="flex-col text-5xl font-bold flex-1 mt-20">Making Christlike Disciple</h1>
@@ -21,4 +21,5 @@
         <div class="flex justify-center text-3xl text-red-600 mt-10 italic"><p>"This site is underconstruction"</p></div>
       </div>
   </div>
+  
 </div>
